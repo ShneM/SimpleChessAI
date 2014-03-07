d6278f626aebead3ed93c415bbecaf7aeff4f930
Trevor M. Sands <TMSands@users.noreply.github.com>
2014-03-07 11:41:32 -0500

    Update data.c

--- data.c
+++ data.c
@@ -2,9 +2,9 @@
  *	Name:	Carly Deming, Trevor Sands
  *	Class:	CS331 - Intro. to Intelligent Systems
  *	File:	data.c
- *	Info.:	Handles the intialization and 
+ *	Info.:	Handles the intialization and
  *		definitions of game parameters.
- *	
+ *
  *	Credit:	Adapted from Tom Kerrigan's Simple
  *		Chess Program (TSCP.)
  *		Copyright 1997, Tom Kerrigan
@@ -15,28 +15,28 @@
 
 
 /* the board representation */
-int color[64];  /* LIGHT, DARK, or EMPTY */
+int color[64];  /* WHITE, BLACK, or EMPTY */
 int piece[64];  /* PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING, or EMPTY */
 int side;  /* the side to move */
 int xside;  /* the side not to move */
 int castle;  /* a bitfield with the castle permissions. if 1 is set,
-                white can still castle kingside. 2 is white queenside.
-				4 is black kingside. 8 is black queenside. */
+              white can still castle kingside. 2 is white queenside.
+              4 is black kingside. 8 is black queenside. */
 int ep;  /* the en passant square. if white moves e2e4, the en passant
-            square is set to e3, because that's where a pawn would move
-			in an en passant capture */
+          square is set to e3, because that's where a pawn would move
+          in an en passant capture */
 int fifty;  /* the number of moves since a capture or pawn move, used
-               to handle the fifty-move-draw rule */
+             to handle the fifty-move-draw rule */
 int hash;  /* a (more or less) unique number that corresponds to the
-              position */
+            position */
 int ply;  /* the number of half-moves (ply) since the
-             root of the search tree */
+           root of the search tree */
 int hply;  /* h for history; the number of ply since the beginning
-              of the game */
+            of the game */
 
 /* gen_dat is some memory for move lists that are created by the move
-   generators. The move list for ply n starts at first_move[n] and ends
-   at first_move[n + 1]. */
+ generators. The move list for ply n starts at first_move[n] and ends
+ at first_move[n + 1]. */
 gen_t gen_dat[GEN_STACK];
 int first_move[MAX_PLY];
 
@@ -44,11 +44,11 @@ int first_move[MAX_PLY];
 int history[64][64];
 
 /* we need an array of hist_t's so we can take back the
-   moves we make */
+ moves we make */
 hist_t hist_dat[HIST_STACK];
 
 /* the engine will search for max_time milliseconds or until it finishes
-   searching max_depth ply. */
+ searching max_depth ply. */
 int max_time;
 int max_depth;
 
@@ -59,7 +59,7 @@ int stop_time;
 int nodes;  /* the number of nodes we've searched */
 
 /* a "triangular" PV array; for a good explanation of why a triangular
-   array is needed, see "How Computers Play Chess" by Levy and Newborn. */
+ array is needed, see "How Computers Play Chess" by Levy and Newborn. */
 move pv[MAX_PLY][MAX_PLY];
 int pv_length[MAX_PLY];
 BOOL follow_pv;
@@ -70,30 +70,30 @@ int hash_side;
 int hash_ep[64];
 
 /* Now we have the mailbox array, so called because it looks like a
-   mailbox, at least according to Bob Hyatt. This is useful when we
-   need to figure out what pieces can go where. Let's say we have a
-   rook on square a4 (32) and we want to know if it can move one
-   square to the left. We subtract 1, and we get 31 (h5). The rook
-   obviously can't move to h5, but we don't know that without doing
-   a lot of annoying work. Sooooo, what we do is figure out a4's
-   mailbox number, which is 61. Then we subtract 1 from 61 (60) and
-   see what mailbox[60] is. In this case, it's -1, so it's out of
-   bounds and we can forget it. You can see how mailbox[] is used
-   in attack() in board.c. */
+ mailbox, at least according to Bob Hyatt. This is useful when we
+ need to figure out what pieces can go where. Let's say we have a
+ rook on square a4 (32) and we want to know if it can move one
+ square to the left. We subtract 1, and we get 31 (h5). The rook
+ obviously can't move to h5, but we don't know that without doing
+ a lot of annoying work. Sooooo, what we do is figure out a4's
+ mailbox number, which is 61. Then we subtract 1 from 61 (60) and
+ see what mailbox[60] is. In this case, it's -1, so it's out of
+ bounds and we can forget it. You can see how mailbox[] is used
+ in attack() in board.c. */
 
 int mailbox[120] = {
-	 -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-	 -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-	 -1,  0,  1,  2,  3,  4,  5,  6,  7, -1,
-	 -1,  8,  9, 10, 11, 12, 13, 14, 15, -1,
-	 -1, 16, 17, 18, 19, 20, 21, 22, 23, -1,
-	 -1, 24, 25, 26, 27, 28, 29, 30, 31, -1,
-	 -1, 32, 33, 34, 35, 36, 37, 38, 39, -1,
-	 -1, 40, 41, 42, 43, 44, 45, 46, 47, -1,
-	 -1, 48, 49, 50, 51, 52, 53, 54, 55, -1,
-	 -1, 56, 57, 58, 59, 60, 61, 62, 63, -1,
-	 -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-	 -1, -1, -1, -1, -1, -1, -1, -1, -1, -1
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1,  0,  1,  2,  3,  4,  5,  6,  7, -1,
+    -1,  8,  9, 10, 11, 12, 13, 14, 15, -1,
+    -1, 16, 17, 18, 19, 20, 21, 22, 23, -1,
+    -1, 24, 25, 26, 27, 28, 29, 30, 31, -1,
+    -1, 32, 33, 34, 35, 36, 37, 38, 39, -1,
+    -1, 40, 41, 42, 43, 44, 45, 46, 47, -1,
+    -1, 48, 49, 50, 51, 52, 53, 54, 55, -1,
+    -1, 56, 57, 58, 59, 60, 61, 62, 63, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1
 };
 
 int mailbox64[64] = {
@@ -109,10 +109,10 @@ int mailbox64[64] = {
 
 
 /* slide, offsets, and offset are basically the vectors that
-   pieces can move in. If slide for the piece is FALSE, it can
-   only move one square in any one direction. offsets is the
-   number of directions it can move in, and offset is an array
-   of the actual directions. */
+ pieces can move in. If slide for the piece is FALSE, it can
+ only move one square in any one direction. offsets is the
+ number of directions it can move in, and offset is an array
+ of the actual directions. */
 
 BOOL slide[6] = {
 	FALSE, FALSE, TRUE, TRUE, TRUE, FALSE
@@ -133,16 +133,16 @@ int offset[6][8] = {
 
 
 /* This is the castle_mask array. We can use it to determine
-   the castling permissions after a move. What we do is
-   logical-AND the castle bits with the castle_mask bits for
-   both of the move's squares. Let's say castle is 1, meaning
-   that white can still castle kingside. Now we play a move
-   where the rook on h1 gets captured. We AND castle with
-   castle_mask[63], so we have 1&14, and castle becomes 0 and
-   white can't castle kingside anymore. */
+ the castling permissions after a move. What we do is
+ logical-AND the castle bits with the castle_mask bits for
+ both of the move's squares. Let's say castle is 1, meaning
+ that white can still castle kingside. Now we play a move
+ where the rook on h1 gets captured. We AND castle with
+ castle_mask[63], so we have 1&14, and castle becomes 0 and
+ white can't castle kingside anymore. */
 
 int castle_mask[64] = {
-	 7, 15, 15, 15,  3, 15, 15, 11,
+    7, 15, 15, 15,  3, 15, 15, 11,
 	15, 15, 15, 15, 15, 15, 15, 15,
 	15, 15, 15, 15, 15, 15, 15, 15,
 	15, 15, 15, 15, 15, 15, 15, 15,
